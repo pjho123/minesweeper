aab60b4a8208074389febc74691c849c4de22b02
Prince Ho <prince.ho@honcho.com>
2019-10-08 18:30:57 +0800

    add timer and actions

--- src/components/mine/MineContainer.js
+++ src/components/mine/MineContainer.js
@@ -7,18 +7,34 @@ class MineContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            x: 16,
+            x: 30,
             y: 16,
             rows: [],
-            mines: 40
+            mines: 99,
+            flagCount: 0,
+            time: '00:00',
+            timeCounter: 0,
+            started: false
         };
         this.generateMines = this.generateMines.bind(this);
         this.handleRightClick = this.handleRightClick.bind(this);
         this.handleLeftClick = this.handleLeftClick.bind(this);
+        this.timerAction = this.timerAction.bind(this);
+        this.handleButtonAction = this.handleButtonAction.bind(this);
+        this.updateRows = this.updateRows.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
         this.generateMines();
+    }
+
+    updateRows(rows) {
+        this.setState({rows: rows});
+
+        setTimeout(() => {
+            this.setState({flagCount: document.querySelectorAll('.flag').length});
+        }, 100)
     }
 
     generateMines() {
@@ -46,7 +62,7 @@ class MineContainer extends Component {
                 }
             });
         });
-        this.setState({rows: rows});
+        this.updateRows(rows);
     }
 
     handleRightClick(cell, rowIndex, cellIndex) {
@@ -65,11 +81,14 @@ class MineContainer extends Component {
         }
 
         rows[rowIndex][cellIndex] = cell;
-        this.setState({rows: rows});
-
+        this.updateRows(rows);
     }
 
     handleLeftClick(cell, rowIndex, cellIndex) {
+        if (!this.state.started) {
+            this.timerAction('start');
+        }
+
         let rows = this.state.rows;
 
         if (cell.state === 'flag') {
@@ -81,23 +100,23 @@ class MineContainer extends Component {
             const flagCount = cellAround.filter(data => data && data.state === 'flag');
 
             if (cell.minesAround === flagCount.length) {
-                cellAround.map((data) => {
+                cellAround.map(data => {
                     if (!data || data.state === 'flag' || data.state === 'open') {
                         return false;
                     }
-                    console.log(data);
 
                     data.state = 'open';
                     rows[data.position.y][data.position.x].state = 'open';
 
                     if (data.minesAround === 0) {
-                        console.log(document.querySelector(`.js-${cell.cellNo}`));
                         document.querySelector(`.js-${data.cellNo}`).click();
                     }
+
+                    return true;
                 });
             }
-            this.setState({rows: rows});
-            return;
+
+            return this.updateRows(rows);
         }
 
         cell.state = 'open';
@@ -107,12 +126,84 @@ class MineContainer extends Component {
             rows = CellLocator.getAllZeroAroundByIndex(rows, rowIndex, cellIndex);
         }
 
-        this.setState({rows: rows});
+        this.updateRows(rows);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const name = target.name;
+
+        this.setState({
+            [name]: parseInt(target.value)
+        });
+    }
+
+    handleButtonAction(action) {
+        switch (action) {
+            case  'play':
+                this.generateMines();
+                break;
+            case  'stop':
+                this.timerAction('stop');
+                break;
+        }
+    }
+
+    timerAction(action) {
+        switch (action) {
+            case  'start':
+                if (this.timer) {
+                    clearInterval(this.timer);
+                }
+
+                this.setState({
+                    time: `00:00`,
+                    timeCounter: 0,
+                    started: true
+                });
+
+                this.timer = setInterval(() => {
+                    const timeCount = this.state.timeCounter + 1;
+                    this.setState({
+                        time: `${(Math.trunc(timeCount / 60)).toString().padStart(2, '0')}:${(timeCount % 60).toString().padStart(2, '0')}`,
+                        timeCounter: timeCount,
+                    });
+
+                }, 1000);
+                break;
+            case 'pause':
+            case 'stop':
+                if (this.timer) {
+                    clearInterval(this.timer);
+                }
+
+                this.setState({started: false});
+                break;
+        }
     }
 
     render() {
         return (
             <div>
+                <p>
+                    <span>Flag: {this.state.flagCount}/{this.state.mines}</span>
+                    <span>Timer: {this.state.time}</span>
+                </p>
+                <p>
+                    <span>
+                        Width: <input type="number" onChange={this.handleChange} name="x" value={this.state.x}/>
+                        Height: <input type="number" onChange={this.handleChange} name="y" value={this.state.y}/>
+                        Mines: <input type="number" onChange={this.handleChange} name="mines" value={this.state.mines}/>
+                    </span>
+                    <button onClick={() => {
+                        this.handleButtonAction('play')
+                    }}>Play
+                    </button>
+                    <button onClick={() => {
+                        this.handleButtonAction('stop')
+                    }}>Stop
+                    </button>
+                </p>
                 {this.state.rows.map((cells, rowIndex) =>
                     <p key={rowIndex}>
                         {cells.map((cell, cellIndex) => {
